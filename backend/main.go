e6ad05af4501d31ac0bbbfa559340a1f9edc6303
tinaba96 <tinaba96.ca@gmail.com>
2024-07-25 23:58:10 -0400

    update

--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,23 @@
 package main
 
+// #cgo CFLAGS: -I/Library/Frameworks/Python.framework/Versions/3.9/include/python3.9
+// #cgo LDFLAGS: -L/Library/Frameworks/Python.framework/Versions/3.9/lib -lpython3.9
+// #include <Python.h>
+
+import (
+	"C"
+	"os"
+	"strconv"
+
+	"gorm.io/gorm"
+)
 import (
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-
-	"github.com/PuerkitoBio/goquery"
 	"github.com/tinaba96/taka-ultimate-fashion/backend/router"
-
-	"github.com/sclevine/agouti"
+	"github.com/tinaba96/taka-ultimate-fashion/backend/scraper"
+	"gorm.io/driver/mysql"
 )
 
 type Product struct {
@@ -30,7 +33,15 @@ type Product struct {
 
 type Category struct {
     ID   uint   `gorm:"primaryKey"`
-    Name string
+	Name string `gorm:"unique"`
+    gorm.Model
+}
+
+type Save struct {
+    ID   uint   `gorm:"primaryKey"`
+	ProductID uint
+	Product Product
+	Status uint
     gorm.Model
 }
 
@@ -53,7 +64,6 @@ func getDBConfig() DBConfig {
     }
 }
 
-
 func main() {
 	// ====== API =======
 	log.Println("start server...")
@@ -61,6 +71,24 @@ func main() {
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
+
+
+	// ====== Python Execution =======
+	// cmd := exec.Command("python3", "deep_learning/keras.py")
+
+    // // コマンドを実行し、標準出力を取得
+    // output, err := cmd.CombinedOutput()
+    // if err != nil {
+    //     fmt.Println("Error:", err)
+    //     return
+    // }
+	// fmt.Println(string(output))
+	// err := python.PyRun_SimpleFile("./deep_learning/keras.py")
+    // if err != nil {
+    //     panic(err.Error)
+    // }
+
+	
 
 	// ====== DB Connections =======
 	// https://qiita.com/Alphard/items/5239c2707b1557f76c9a#maingo
@@ -71,115 +99,27 @@ func main() {
 	if errdb != nil {
 		panic("failed to connect database")
 	}
+
+
 	// Migrate the schema
-	db.AutoMigrate(&Product{}, &Category{})
-	// Master Category Data
-	db.Create(&Category{Name: "T-SHIRTS"})
-    db.Create(&Category{Name: "SWIMWEAR"})
-	db.Create(&Category{Name: "SHIRTS"})
-	db.Create(&Category{Name: "JEANS"})
-    db.Create(&Category{Name: "SHORTS"})
-    db.Create(&Category{Name: "PANTS"})
-    db.Create(&Category{Name: "GRAPHIC T-SHIRTS"})
-    db.Create(&Category{Name: "ACTIVEWEAR"})
-
-	// var product Product
-	// db.First(&product, "Name = ?", "TSHIRT") // find product with code D42
-	// fmt.Println(product)
+	// if !db.Migrator().HasTable(&Product{}) && !db.Migrator().HasTable(&Category{}) && !db.Migrator().HasTable(&Save{}) {
+	// db.AutoMigrate(&Product{}, &Category{}, &Save{})
+		// Master Category Data
+	db.FirstOrCreate(&Category{Name: "T-SHIRTS"})
+	db.FirstOrCreate(&Category{Name: "SWIMWEAR"})
+	db.FirstOrCreate(&Category{Name: "SHIRTS"})
+	db.FirstOrCreate(&Category{Name: "JEANS"})
+	db.FirstOrCreate(&Category{Name: "SHORTS"})
+	db.FirstOrCreate(&Category{Name: "PANTS"})
+	db.FirstOrCreate(&Category{Name: "GRAPHIC T-SHIRTS"})
+	db.FirstOrCreate(&Category{Name: "ACTIVEWEAR"})
+	// }
 
 
-
-	// ====== start scraping =======
-	// url := "https://oldnavy.gapcanada.ca/browse/category.do?cid=5249&mlink=5155,1,m_5" // specify the URL
-	url := "https://oldnavy.gapcanada.ca/browse/category.do?cid=5199&mlink=5155,1,m_6"
-
-	// driver := agouti.ChromeDriver()                                // Start the driver
-	driver := agouti.ChromeDriver(
-		agouti.ChromeOptions("args", []string{
-			"--headless",
-			"--no-sandbox",
-			"--disable-dev-shm-usage",
-			"--disable-gpu",
-			"--whitelisted-ips",
-			"--detach",
-		}),
-		// agouti.Debug,  // Local only
-	)
-
-	log.Println("start")
-	err := driver.Start()
-	if err != nil {
-		log.Printf("Error starting driver: %v", err.Error())
-	}
-	defer driver.Stop()
-
-	page, err := driver.NewPage(agouti.Browser("chrome")) // Start Chrome and return a page type value (session)
-	if err != nil {
-		log.Printf("Error creating new page: %v", err.Error())
-	}
-	time.Sleep(3 * time.Second)
-
-	err = page.Navigate(url) // Access the specified URL
-	if err != nil {
-		log.Printf("Error navigating to job post link: %v", err.Error())
-	}
-	time.Sleep(3 * time.Second)
-	log.Println("end")
-
-	// Scroll down to load more content
-	for i := 0; i < 5; i++ {
-		err = page.RunScript("window.scrollTo(0, document.body.scrollHeight);", nil, nil)
-		if err != nil {
-			log.Fatalf("Failed to scroll: %v", err)
-		}
-		time.Sleep(3 * time.Second) // Wait for new content to load
-	}
-
-	log.Println("Retrieving HTML content...")
-
-	curContentsDom, err := page.HTML()
-	if err != nil {
-		log.Printf("Failed to get html: %v", err)
-	}
-
-	// Write the HTML to output.txt
-	err = os.WriteFile("output.txt", []byte(curContentsDom), 0644)
-	if err != nil {
-		log.Printf("Failed to write to file: %v", err)
-		return
-	}
-	log.Println("HTML content written to output.txt")
-
-	readerCurContents := strings.NewReader(curContentsDom)
-	contentsDom, _ := goquery.NewDocumentFromReader(readerCurContents) // Get the DOM of the currently open page in the browser
+	scraper.Scraper()
+		
 	
-
-	contentsDom.Find(".cat_product-image").Each(func(i int, s *goquery.Selection) {
-		imgSrc, _ := s.Find("img").Attr("src")
-		productName := s.Next().Find("div.category-page-1r1wcud").Text()
-		price := s.Next().Next().Find("div.product-price__highlight").Text()
-
-		var productCategory Category
-		// db.FirstOrCreate(&productCategory, Category{Name: "T-SHIRTS"}) 
-		db.FirstOrCreate(&productCategory, Category{Name: "JEANS"}) 
-
-		product := Product{
-			Name:     productName,
-			Price:    price,
-			ImageURL: imgSrc,
-			CategoryID: productCategory.ID,
-		}
-
-		if err := db.Create(&product).Error; err != nil {
-			log.Printf("Failed to save product: %v", err)
-		} else {
-			fmt.Printf("Saved Product: %s, Price: %s, Image Src: %s\n", productName, price, imgSrc)
-		}
-
-		fmt.Printf("Product: %s, Price: %s, Image Src: %s\n", productName, price, imgSrc)
-	})
-	
-	
+	// db.FirstOrCreate(&Save{ProductID: 12, Status: 1}) 
 	log.Println("--- DONE ---")
 
 }
