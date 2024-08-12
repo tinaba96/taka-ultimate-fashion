5cfa30474c759e570acc514204be028ab376c7c4
tinaba96 <tinaba96.ca@gmail.com>
2024-08-11 23:35:39 -0400

    update

--- frontend/src/layouts/header.tsx
+++ frontend/src/layouts/header.tsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
     <header>
-          <div className='py-6'>
-        <h1 className="underline font-serif hover:font-sans text-5xl italic header-title pt-5 font-bold tracking-tight text-gray-900">Taka's Ultimate Fashion</h1>
+        <div className='header-comp py-6'>
+        <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services" className="nav-link">Services</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services" className="nav-link">Services</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+            <h1 className="header-comp-title font-sans text-2xl font-bold tracking-tight text-gray-900">Taka's Ultimate Fashion</h1>
         </div>
+        <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services" className="nav-link">Services</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
