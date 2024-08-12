6db36f834ecd27fe45368e2c2341b052c80cf7fd
tinaba96 <tinaba96.ca@gmail.com>
2024-08-12 01:26:53 -0400

    update

--- frontend/src/layouts/header.tsx
+++ frontend/src/layouts/header.tsx
@@ -5,56 +5,59 @@ const Header: React.FC = () => {
   return (
     <header>
         <div className='header-comp py-6'>
+          <div className='top-header-comp py-6'>
+          <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-link">Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
+          </ul>
+        </nav>
         <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/services" className="nav-link">Services</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/services" className="nav-link">Services</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-            <h1 className="header-comp-title font-sans text-2xl font-bold tracking-tight text-gray-900">Taka's Ultimate Fashion</h1>
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">WHATS NEW</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-link">Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+              <h1 className="header-comp-title font-sans text-2xl font-bold tracking-tight text-gray-900">Taka's Ultimate Fashion</h1>
+              </div><div>
+          <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-link">Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+          </div>
         </div>
-        <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/services" className="nav-link">Services</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">Contact</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
