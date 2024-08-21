e9bcb19249607974999bd411c4a1577fb01dda9a
tinaba96 <tinaba96.ca@gmail.com>
2024-08-21 01:03:08 -0400

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,20 @@ import './styles/tailwind.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Header from './layouts/header';
+import Sidebar from './layouts/sidebar';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div style={{ paddingTop: '10%' }} className="app-container">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Sidebar />
+      </div>
     </Router>
 
   );
